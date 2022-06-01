e4eaf353b084c7529be1b20bdc434ed997d44ac5
Yannic Neßhöver <yne@ordix.de>
2022-06-01 16:32:51 +0200

    Added Config files and package.json

---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+    ...require('./node_modules/ordix-eslint-and-prettier-config/.prettierrc.json')
+};
---
+++ src/LintTest.ts
@@ -0,0 +1,5 @@
+export class AdminViewComponent {
+    public s: String;
+
+    constructor() {}
+}
